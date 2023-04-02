60f200267efe4b6d18e01c3213a91e4ac68d247e
Rashmika9712 <rashmikamadushansamarakoon@gmail.com>
2023-04-02 19:42:24 +0530

    Addedd DTO

--- TodoApi/Models/TodoItemDTO.cs
+++ TodoApi/Models/TodoItemDTO.cs
@@ -1,10 +1,9 @@
 ﻿namespace TodoApi.Models
 {
-    public class TodoItem
+    public class TodoItemDTO
     {
         public int Id { get; set; }
         public string? Name { get; set; }
         public bool IsComplete { get; set; }
-        public string? Secret { get; set; }
     }
 }
